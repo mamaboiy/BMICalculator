2bf40e168abf12ed45df33e0535357ac4759cb25
mamaboiy <163087204+mamaboiy@users.noreply.github.com>
2024-05-03 10:29:47 +0600

    Add files via upload

---
+++ style.js
@@ -0,0 +1,35 @@
+
+  
+  function calculateBMI() {
+    var heightInput = document.getElementById("height");
+    var weightInput = document.getElementById("weight");
+    var resultDiv = document.getElementById("result");
+  
+    var height = parseFloat(heightInput.value);
+    var weight = parseFloat(weightInput.value);
+  
+    if (isNaN(height) || isNaN(weight)) {
+      resultDiv.innerHTML = "Please enter valid height and weight. " ;
+      let myimage = doument.getElementById("myimage");
+      myimage.setAttribute("src","./exclamation-diamond.svg");
+      return;
+    }
+  
+   
+
+    var bmi = weight / (height * height);
+    var category = "";
+  
+    if (bmi < 18.5) {
+      category = "Underweight";
+    } else if (bmi < 25) {
+      category = "Normal weight";
+    } else if (bmi < 30) {
+      category = "Overweight";
+    } else {
+      category = "Obese";
+    }
+  
+    resultDiv.innerHTML = "Your BMI is " + bmi.toFixed(2) +" [" + category + "]";
+  }
+  
